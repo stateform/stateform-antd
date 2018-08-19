0e6c8bd260781d2b5fd663639547a25a0b3a3be8
WilliamLeung <superbill2012@qq.com>
2018-08-19 11:05:40 +0800

    build:change the name of css file

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -32,7 +32,7 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = '[name].css';
+const cssFilename = 'stateform-antd.css';
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
